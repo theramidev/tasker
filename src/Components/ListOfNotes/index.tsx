b6975a23d616fdebdb225c5d93911c701f162182
David Eduardo Ramirez Soto <david.ramirez1448@gmail.com>
2020-04-03 20:45:16 -0400

    se arreglo el ordenamiento de el listado

--- src/Components/ListOfNotes/index.tsx
+++ src/Components/ListOfNotes/index.tsx
@@ -1,5 +1,13 @@
 import React, {FC} from 'react';
-import {View, Text, FlatList, Image, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  RecyclerViewBackedScrollView,
+} from 'react-native';
 import {styles} from './styles';
 
 export const ListOfNotes: FC = () => {
@@ -34,6 +42,33 @@ export const ListOfNotes: FC = () => {
       placeat. Earum, quos maxime.`,
       image: 'https://wallpapercave.com/wp/wp5065028.jpg',
     },
+    {
+      id: 5,
+      title: 'chingada',
+      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.`,
+      image: 'https://wallpapercave.com/wp/wp5065028.jpg',
+    },
+    {
+      id: 6,
+      title: 'chingada',
+      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit.`,
+      image: 'https://wallpapercave.com/wp/wp5065028.jpg',
+    },
+    {
+      id: 7,
+      title: 'Pan',
+      text: `Comprar pan`,
+      image: 'https://wallpapercave.com/wp/wp5065028.jpg',
+    },
+    {
+      id: 8,
+      title: 'Titulo',
+      text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus
+      doloribus facilis sit eveniet possimus modi, consequuntur culpa? At
+      officia blanditiis aperiam assumenda nesciunt aut, voluptas culpa
+      placeat. Earum, quos maxime.`,
+      image: 'https://wallpapercave.com/wp/wp5065028.jpg',
+    },
   ];
 
   const cutText = (txt: string) => {
@@ -49,7 +84,7 @@ export const ListOfNotes: FC = () => {
       key={item.id}
       style={[
         styles.containerItem,
-        {marginTop: index === 0 || index === 1 ? 10 : 0},
+        {marginTop: index === 0 ? 10 : 0},
       ]}>
       <Image style={styles.image} source={{uri: item.image}} />
       <View style={styles.info}>
@@ -60,15 +95,31 @@ export const ListOfNotes: FC = () => {
   );
 
   return (
-    <FlatList
-      style={{marginHorizontal: 5}}
-      data={mocks}
-      renderItem={(item) => cardNote(item)}
-      numColumns={2}
-      columnWrapperStyle={{
-        flex: 1,
-        justifyContent: 'space-between',
-      }}
-    />
+    <ScrollView style={{marginBottom: 60}}>
+      <View style={{flexDirection: 'row', flexWrap: 'wrap'}}>
+        <FlatList
+          style={{marginHorizontal: 5}}
+          data={mocks.filter((item, index) => (index % 2 ? false : true))}
+          renderItem={(item) => cardNote(item)}
+          scrollEnabled={false}
+          /* numColumns={2}
+        columnWrapperStyle={{
+          flex: 1,
+          justifyContent: 'space-between',
+        }} */
+        />
+        <FlatList
+          style={{marginHorizontal: 5}}
+          data={mocks.filter((item, index) => (index % 2 ? true : false))}
+          renderItem={(item) => cardNote(item)}
+          scrollEnabled={false}
+          /* numColumns={2}
+        columnWrapperStyle={{
+          flex: 1,
+          justifyContent: 'space-between',
+        }} */
+        />
+      </View>
+    </ScrollView>
   );
 };
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {Text, View, TouchableOpacity} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -7,7 +7,7 @@ import {FloatingAction} from 'react-native-floating-action';
 import {styles} from './styles';
 import {ListOfNotes} from '../../Components/ListOfNotes';
 import {Layout} from '../../Components/Layout';
-import { Header } from '../../Components/Header';
+import {Header} from '../../Components/Header';
 import {IProps} from './interfaces/IProps';
 
 class HomeScreen extends Component<IProps, {}> {
@@ -22,19 +22,25 @@ class HomeScreen extends Component<IProps, {}> {
     ];
 
     return (
-      <Layout>
-        <Header navigation={this.props.navigation} title="Tasker" mode="none" />
-        <View style={styles.container}>
-          <ListOfNotes />
-          <FloatingAction
-            actions={options}
-            onPressItem={(name) => {
-              console.log(`selected button: ${name}`);
-            }}
-            animated={!__DEV__}
+      <Fragment>
+        <Layout>
+          <Header
+            navigation={this.props.navigation}
+            title="Tasker"
+            mode="none"
           />
-        </View>
-      </Layout>
+          <View style={styles.container}>
+            <ListOfNotes />
+          </View>
+        </Layout>
+        <FloatingAction
+          actions={options}
+          onPressItem={(name) => {
+            console.log(`selected button: ${name}`);
+          }}
+          animated={!__DEV__}
+        />
+      </Fragment>
     );
   }
 }
