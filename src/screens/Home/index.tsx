30769a3df57ff51af212aa23db213cda30a1da8d
Miguel Ramírez <miguelramidev@gmail.com>
2020-04-08 17:15:55 -0400

    Se quitó el botton del home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
-import { Button } from 'react-native';
 import { IProps } from './interfaces/IProps';
 import { IState } from './interfaces/IState';
 
 import { Layout } from '../../components/Layout';
 import { Header } from '../../components/Header';
-import Notification from '../../utils/notifications';
 
 
 class HomeScreen extends Component<IProps, IState> {
-
-    notification = () => {
-        Notification(
-            2,
-            'Sub Text',
-            '#tag',
-            'Title',
-            'Message',
-            new Date(Date.now() + 10 * 1000)
-        );
-    }
 
     render() {
         return(
@@ -29,8 +16,6 @@ class HomeScreen extends Component<IProps, IState> {
                     title="Tasker"
                     mode="menu"
                 />
-
-                <Button title="Presiona aquí" onPress={this.notification} />
             </Layout>
         )
     }
